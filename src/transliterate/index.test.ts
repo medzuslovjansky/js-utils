757877e48a2834c430b7110ab06662c0e3ed3199
Yaroslav Serhieiev <noomorph@gmail.com>
2023-06-05 15:09:48 +0300

    fix(transliterate): handle Cyrillic back and forth (#4)

--- src/transliterate/index.test.ts
+++ src/transliterate/index.test.ts
@@ -1,13 +1,19 @@
 import { transliterate } from './index';
 
-const source = `\
+const latin = `\
 Na vȯzvyšenosti ovca, ktora ne iměla vȯlnų, uviděla konjev. Pŕvy tęgal tęžky voz, vtory nosil veliko brěmę, tretji brzo vozil mųža.
 Ovca rěkla konjam: «Boli mně sŕdce, kȯgda viđų, kako člověk vladaje konjami.»
 Konji rěkli: «Slušaj, ovco, nam boli sŕdce, kȯgda vidimo ovo: mųž, gospodaŕ, bere tvojų vȯlnų, da by iměl dlja sebe teplo paĺto. A ovca jest bez vȯlny.»
 Uslyšavši to, ovca izběgla v råvninų. | Odjezd.`;
 
-describe('abcd', () => {
-  test.each([
+const cyrillic = `\
+На возвышености овца, ктора не имѣла вълнѫ, увидѣла коњев. Прьвы тѧгал тѧжкы воз, вторы носил велико брємѧ, третји брзо возил мѫжа.
+Овца рѣкла коням: «Боли мнє срьдце, къгда виџу, како чловѣк владаје коньами.»
+Конји рѣкли: «Слушай, овцо, нам боли срьдце, къгда видимо ово: мѫж, господарь, бере твоѭ вълнѫ, да бы имѣл дља себе тепло пальто. А овца ѥсть без вълны.»
+Услышавши то, овца избѣгла в рӑвнинѹ. | Одјезд.`;
+
+describe('transliterate to', () => {
+  describe.each([
     ['art-Cyrl-x-interslv'],
     ['art-Cyrl-x-interslv-etym'],
     ['art-Cyrl-x-interslv-iotated'],
@@ -29,13 +35,31 @@ describe('abcd', () => {
     ['art-Latn-x-interslv-southern'],
     ['art-x-interslv-fonipa'],
     ['art-x-interslv'],
-  ] as const)('transliterate(text, %j)', (bcp47) => {
-    expect(transliterate(source, bcp47)).toMatchSnapshot();
+  ] as const)('%j', (bcp47) => {
+    test('a latin text', () => {
+      expect(transliterate(latin, bcp47)).toMatchSnapshot();
+    });
+
+    test('a cyrillic text', () => {
+      expect(transliterate(cyrillic, bcp47)).toMatchSnapshot();
+    });
   });
 
+  test.each([
+    ['art-Latn-x-interslv'],
+    ['art-Cyrl-x-interslv'],
+    ['art-Glag-x-interslv'],
+  ] as const)(
+    'should work equally from Latin and Cyrillic scripts to %j',
+    (bcp47) => {
+      const latn = transliterate(latin, bcp47);
+      const cyrl = transliterate(cyrillic, bcp47);
+
+      expect(latn).toEqual(cyrl);
+    },
+  );
+
   test('unknown code', () => {
-    expect(() =>
-      transliterate(source, 'en' as any),
-    ).toThrowErrorMatchingSnapshot();
+    expect(() => transliterate('', 'en' as any)).toThrowErrorMatchingSnapshot();
   });
 });
