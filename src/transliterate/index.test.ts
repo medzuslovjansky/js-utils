585959b684350a3c2a2b604a4c465c127727ddb3
Yaroslav Serhieiev <noomorph@gmail.com>
2023-07-13 09:31:34 +0300

    fix: transliteration of non-combined accent marks

--- src/transliterate/index.test.ts
+++ src/transliterate/index.test.ts
@@ -4,13 +4,13 @@ const latin = `\
 Na vȯzvyšenosti ovca, ktora ne iměla vȯlnų, uviděla konjev. Pŕvy tęgal tęžky voz, vtory nosil veliko brěmę, tretji brzo vozil mųža.
 Ovca rěkla konjam: «Boli mně sŕdce, kȯgda viđų, kako člověk vladaje konjami.»
 Konji rěkli: «Slušaj, ovco, nam boli sŕdce, kȯgda vidimo ovo: mųž, gospodaŕ, bere tvojų vȯlnų, da by iměl dlja sebe teplo paĺto. A ovca jest bez vȯlny.»
-Uslyšavši to, ovca izběgla v råvninų. | Odjezd.`;
+Uslyšavši to, ovca izběgla v råvninų. | Odjezd. T́ma.`;
 
 const cyrillic = `\
 На возвышености овца, ктора не имѣла вълнѫ, увидѣла коњев. Прьвы тѧгал тѧжкы воз, вторы носил велико брємѧ, третји брзо возил мѫжа.
 Овца рѣкла коням: «Боли мнє срьдце, къгда виџу, како чловѣк владаје коньами.»
 Конји рѣкли: «Слушай, овцо, нам боли срьдце, къгда видимо ово: мѫж, господарь, бере твоѭ вълнѫ, да бы имѣл дља себе тепло пальто. А овца ѥсть без вълны.»
-Услышавши то, овца избѣгла в рӑвнинѹ. | Одјезд.`;
+Услышавши то, овца избѣгла в рӑвнинѹ. | Одјезд. Тьма.`;
 
 describe('transliterate to', () => {
   describe.each([
