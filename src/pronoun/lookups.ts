522d43fe748b1ec74ae476cb767a377e2a2eb079
Yaroslav Serhieiev <noomorph@gmail.com>
2024-01-01 14:28:43 +0200

    fix(pronouns): declension of sam and ono

---
+++ src/pronoun/lookups.ts
@@ -0,0 +1,52 @@
+export const FIRST_PERSON = [
+  'ja',
+  'mene',
+  'me',
+  'mne',
+  'mi',
+  'mnoju',
+  'my',
+  'nas',
+  'nam',
+  'nami',
+];
+
+export const SECOND_PERSON = [
+  'ty',
+  'tebe',
+  'te',
+  'tobe',
+  'ti',
+  'toboju',
+  'vy',
+  'vas',
+  'vam',
+  'vami',
+];
+
+export const THIRD_PERSON = [
+  'on',
+  'jego',
+  'go',
+  'je',
+  'jemu',
+  'mu',
+  'njim',
+  'ona',
+  'ono',
+  'ju',
+  'jej',
+  'jeju',
+  'njeju',
+  'oni',
+  'one',
+  'jih',
+  'jim',
+  'njimi',
+];
+
+export const REFLEXIVE = ['sebe', 'se', 'sobe', 'si', 'soboju'];
+
+export const ALONE = ['sam', 'sama', 'samo', 'sami', 'same'];
+
+export const POSSESSIVE = ['jih', 'jej', 'jego'];
