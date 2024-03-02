25b127e1dc154998821e1b996c4be2ac158edcc2
Yaroslav Serhieiev <noomorph@gmail.com>
2024-03-02 12:03:19 +0200

    test(html): use different verbs

--- src/__utils__/html/verb.test.ts
+++ src/__utils__/html/verb.test.ts
@@ -7,14 +7,14 @@ describe('renderers', () => {
   describe('HTML', () => {
     describe('verb conjugation', () => {
       test('render', () => {
-        const dreti = conjugationVerb('drěti', '(dere)')!;
-        expect(verb.render(dreti)).toMatchSnapshot();
+        const example = conjugationVerb('dověděti sę', '(dově)')!;
+        expect(verb.render(example)).toMatchSnapshot();
       });
 
       test('renderDiff', () => {
-        const before = conjugationVerb('dreti', '(dere)')!;
+        const before = conjugationVerb('dověděti sę', '(dově)')!;
         // we choose a different verb to show the diff
-        const after = conjugationVerb('trěti', '(tre)')!;
+        const after = conjugationVerb('pověděti', '(pově)')!;
         expect(verb.renderDiff(before, after)).toMatchSnapshot();
       });
     });
