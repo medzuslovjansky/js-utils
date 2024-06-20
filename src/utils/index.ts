9e2b65d613b36b8c34de8e2fff52dc9f5b0fc04d
Yaroslav Serhieiev <noomorph@gmail.com>
2024-06-20 11:00:27 +0300

    refactor: prepare matcher utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from './areArraysEqual';
 export * from './compactArray';
+export * from './matchStart';
+export * from './matchEnd';
 export * from './memoizeLastCall';
 export * from './removeBrackets';
 export * from '../common/stripDiacritics';
