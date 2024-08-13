ec8ca0ba3cccd77778fd685572199dd536691b26
Yaroslav Serhieiev <noomorph@gmail.com>
2024-08-13 21:57:16 +0300

    feat: add transliterate2 subsystem

--- src/transliterate2/glag2latn.ts
+++ src/transliterate2/glag2latn.ts
@@ -1,122 +1,89 @@
-// noinspection NonAsciiCharacters,JSNonASCIINames
-
 import { createReplacer } from './createReplacer';
 import { Glagolitic } from '../constants';
 import { replaceByWord } from './replaceByWord';
 
-const ciafy = (text: string) =>
-  text.replace(/([cs])i([aåeěęėioȯuųùy])/g, '$1|j$2');
+const punctuate = createReplacer({
+  ';': '?',
+  '։': '.',
+  '·': ',',
+  '⁖': '(',
+  '჻': ')',
+  '⸪': '"',
+  '··': '!',
+  '⸬': ':',
+  '⁘': ';',
+  '···': '...',
+});
 
 const softener = createReplacer({
-  ď: 'dь',
-  ľ: 'lь',
-  lj: 'lь',
-  ń: 'nь',
-  nj: 'nь',
-  ŕ: 'rь',
-  ś: 'sь',
-  ť: 'tь',
-  ź: 'zь',
+  dь: 'ď',
+  lь: 'ľ',
+  nь: 'ń',
+  rь: 'ŕ',
+  sь: 'ś',
+  tь: 'ť',
+  zь: 'ź',
+  ьr: 'ŕ',
+  ȯr: 'r',
 });
 
-const punctuate = createReplacer({
-  '?': ';',
-  '.': '։',
-  ',': '·',
-  '(': '⁖',
-  ')': '჻',
-  '"': '⸪',
-  '!': '··',
-  ':': '⸬',
-  ';': '⁘',
-  '...': '···',
-  '…': '···',
+const yerify = (word: string) =>
+  word.replace(/([dlnrstz])ь([cčklnr])/, '$1ė$2');
+
+const deglagolify = createReplacer({
+  [Glagolitic.Azu]: 'a',
+  [Glagolitic.Otu]: 'å',
+  [Glagolitic.Buky]: 'b',
+  [Glagolitic.Tsi]: 'c',
+  [Glagolitic.Chrivi]: 'č',
+  [Glagolitic.Chrivi + Glagolitic.Yeri]: 'ć',
+  [Glagolitic.Shta]: 'ć',
+  [Glagolitic.Dobro]: 'd',
+  [Glagolitic.Djervi]: 'đ',
+  [Glagolitic.Dzelo]: 'ѕ',
+  [Glagolitic.Yestu]: 'e',
+  [Glagolitic.Yati]: 'ě',
+  [Glagolitic.Small_Yus]: 'ę',
+  [Glagolitic.Yeri]: 'ь',
+  [Glagolitic.Fritu]: 'f',
+  [Glagolitic.Glagoli]: 'g',
+  [Glagolitic.Heru]: 'h',
+  [Glagolitic.Spidery_Ha]: 'h',
+  [Glagolitic.I]: 'i',
+  [Glagolitic.Izhe]: 'j',
+  [Glagolitic.Initial_Izhe]: 'j',
+  [Glagolitic.Trokutasti_A]: 'ja',
+  [Glagolitic.Iotated_Small_Yus]: 'ję',
+  [Glagolitic.Yo]: 'jo',
+  [Glagolitic.Yu]: 'ju',
+  [Glagolitic.Iotated_Big_Yus]: 'jų',
+  [Glagolitic.Kako]: 'k',
+  [Glagolitic.Ljudije]: 'l',
+  [Glagolitic.Myslite]: 'm',
+  [Glagolitic.Latinate_Myslite]: 'm',
+  [Glagolitic.Nashi]: 'n',
+  [Glagolitic.Onu]: 'o',
+  [Glagolitic.Yeru]: 'ȯ',
+  [Glagolitic.Pokoji]: 'p',
+  [Glagolitic.Ritsi]: 'r',
+  [Glagolitic.Slovo]: 's',
+  [Glagolitic.Sha]: 'š',
+  [Glagolitic.Tvrido]: 't',
+  [Glagolitic.Fita]: 'θ',
+  [Glagolitic.Uku]: 'u',
+  [Glagolitic.Big_Yus]: 'ų',
+  [Glagolitic.Izhitsa]: 'ù',
+  [Glagolitic.Vedi]: 'v',
+  [Glagolitic.Yeri + Glagolitic.Izhe]: 'y',
+  [Glagolitic.Yeru + Glagolitic.Izhe]: 'y',
+  [Glagolitic.Zemlja]: 'z',
+  [Glagolitic.Zhivete]: 'ž',
 });
 
-const glagolify = createReplacer({
-  a: Glagolitic.Azu, // ⰰ
-  å: Glagolitic.Otu, // ⱉ
-  b: Glagolitic.Buky, // ⰱ
-  c: Glagolitic.Tsi, // ⱌ
-  // "ć": Glagolitic.Chrivi + Glagolitic.Yeri, // ⱍⱐ
-  ć: Glagolitic.Shta, // ⱋ
-  č: Glagolitic.Chrivi, // ⱍ
-  d: Glagolitic.Dobro, // ⰴ
-  đ: Glagolitic.Djervi, // ⰼ
-  ѕ: Glagolitic.Dzelo, // ⰷ
-  dž: Glagolitic.Dobro + Glagolitic.Zhivete, // ⰴⰶ
-  e: Glagolitic.Yestu, // ⰵ
-  ě: Glagolitic.Yati, // ⱑ
-  ę: Glagolitic.Small_Yus, // ⱔ
-  ė: Glagolitic.Yeri, // ⱐ
-  ėj: Glagolitic.Yeri + Glagolitic.Initial_Izhe, // ⱐⰹ
-  ь: Glagolitic.Yeri, // ⰴⱐ
-  ьj: Glagolitic.Yeri + Glagolitic.Initial_Izhe, // ⱐⰹ
-  f: Glagolitic.Fritu, // ⱇ
-  g: Glagolitic.Glagoli, // ⰳ
-  h: Glagolitic.Heru, // ⱈ
-  i: Glagolitic.I, // ⰻ
-  j: Glagolitic.Izhe, // ⰹ
-  jj: Glagolitic.Izhe + Glagolitic.Initial_Izhe, // ⰹⰺ
-  '|j': Glagolitic.Initial_Izhe, // ⰺ
-  '’j': Glagolitic.Initial_Izhe, // ⰺ
-  ję: Glagolitic.Iotated_Small_Yus, // ⱗ
-  jo: Glagolitic.Yo, // ⱖ
-  ju: Glagolitic.Yu, // ⱓ
-  jų: Glagolitic.Iotated_Big_Yus, // ⱙ
-
-  // "ja": Glagolitic.Trokutasti_A, // ⱝ
-  // "ja": "ⰹⰰ", zavisi od města?
-  // "je": Glagolitic.Initial_Izhe + Glagolitic.Yestu, // ⰺⰵ
-  // "ji": Glagolitic.Initial_Izhe + Glagolitic.I, // ⰺⰻ
-  // "jy": Glagolitic.Initial_Izhe + Glagolitic.Izhe + Glagolitic.Yeru, // ⰺⰹⱏ
-
-  k: Glagolitic.Kako, // ⰽ
-  l: Glagolitic.Ljudije, // ⰾ
-  // TODO: return regular Myslite
-  m: Glagolitic.Latinate_Myslite, // ⰿ
-  // TODO: return regular Myslite
-  mj: Glagolitic.Latinate_Myslite + Glagolitic.Initial_Izhe, // ⰿⰺ
-  n: Glagolitic.Nashi, // ⱀ
-  o: Glagolitic.Onu, // ⱁ
-  ȯ: Glagolitic.Yeru, // ⱏ
-  ȯj: Glagolitic.Yeru + Glagolitic.Initial_Izhe, // ⱏⰹ
-  p: Glagolitic.Pokoji, // ⱂ
-  pj: Glagolitic.Pokoji + Glagolitic.Initial_Izhe, // ⱂⰺ
-  r: Glagolitic.Ritsi, // ⱃ
-  s: Glagolitic.Slovo, // ⱄ
-  sj: Glagolitic.Slovo + Glagolitic.Initial_Izhe, // ⱄⰺ
-  š: Glagolitic.Sha, // ⱎ
-  t: Glagolitic.Tvrido, // ⱅ
-  tj: Glagolitic.Tvrido + Glagolitic.Initial_Izhe, // ⱅⰺ
-  θ: Glagolitic.Fita, // ⱚ
-  u: Glagolitic.Uku, // ⱆ
-  ų: Glagolitic.Big_Yus, // ⱘ
-  ù: Glagolitic.Izhitsa, // ⱛ
-  v: Glagolitic.Vedi, // ⰲ
-  vj: Glagolitic.Vedi + Glagolitic.Initial_Izhe, // ⰲⰺ
-  y: Glagolitic.Yeru + Glagolitic.Izhe, // ⱏⰹ
-  // "y": Glagolitic.Yeri + Glagolitic.Izhe, // ⱐⰹ
-  // "y|": Gloagolitic.Yeri + Glagolitic.Izhe + Glagolitic.Initial_Izhe, // ⱐⰹⰺ
-  // "y|": Glagolitic.Yeru + Glagolitic.Izhe + Glagolitic.Initial_Izhe, // ⱏⰹⰺ
-  z: Glagolitic.Zemlja, // ⰸ
-  ž: Glagolitic.Zhivete, // ⰶ
-});
-
-const latn2glag4word = (word: string) => glagolify(ciafy(softener(word)));
-
-export function latn2glag(text: string): string {
-  return punctuate(replaceByWord(text, latn2glag4word));
+function glag2latn4word(word: string): string {
+  return softener(yerify(deglagolify(word)));
 }
 
-// Extra rules:
-
-// [ⱏⱐⰹ]ⰹ → [ⱏⱐⰹ]ⰺ
-
-// [ⱌⱄ]ⰻ[ⰰⰵⱁⱆ] → [ⱌⱄ]ⰺ[ⰰⰵⱁⱆ]
-// Pytanje: sjesti - Initial Izhe (з'їсти)
-
-/*
-
-
-*/
+export function glag2latn(text: string): string {
+  return punctuate(replaceByWord(text, glag2latn4word));
+}
--- src/transliterate2/latn2glag.ts
+++ src/transliterate2/latn2glag.ts
@@ -3,11 +3,27 @@
 import { createReplacer } from './createReplacer';
 import { Glagolitic } from '../constants';
 import { replaceByWord } from './replaceByWord';
+import { syllabicR } from './syllabicR';
 
-const ciafy = (text: string) =>
-  text.replace(/([cs])i([aåeěęėioȯuųùy])/g, '$1’j$2');
+export interface Latn2GlagOptions {
+  /** @default false */
+  latinateMyslite?: boolean;
+  /** @default true */
+  shta?: boolean;
+}
 
-const softener = createReplacer({
+function optionsToInt({ latinateMyslite, shta }: Required<Latn2GlagOptions>) {
+  const b0 = latinateMyslite ? 1 : 0;
+  const b1 = shta ? 2 : 0;
+
+  return b0 | b1;
+}
+
+type Glagolitizer = (word: string) => string;
+
+const MEMO: Record<number, Glagolitizer> = {};
+
+const desoftener = createReplacer({
   ď: 'dь',
   ľ: 'lь',
   lj: 'lь',
@@ -17,6 +33,7 @@ const softener = createReplacer({
   ś: 'sь',
   ť: 'tь',
   ź: 'zь',
+  Ŕ: 'ьr',
 });
 
 const punctuate = createReplacer({
@@ -36,87 +53,84 @@ const punctuate = createReplacer({
 });
 
 const izhefy = createReplacer({
-  [Glagolitic.Yeru + Glagolitic.Izhe]:
-    Glagolitic.Yeru + Glagolitic.Initial_Izhe,
   [Glagolitic.Yeri + Glagolitic.Izhe]:
     Glagolitic.Yeri + Glagolitic.Initial_Izhe,
   [Glagolitic.Izhe + Glagolitic.Izhe]:
     Glagolitic.Izhe + Glagolitic.Initial_Izhe,
 });
 
-const glagolify = createReplacer({
-  a: Glagolitic.Azu, // ⰰ
-  å: Glagolitic.Otu, // ⱉ
-  b: Glagolitic.Buky, // ⰱ
-  c: Glagolitic.Tsi, // ⱌ
-  // "ć": Glagolitic.Chrivi + Glagolitic.Yeri, // ⱍⱐ
-  ć: Glagolitic.Shta, // ⱋ
-  č: Glagolitic.Chrivi, // ⱍ
-  d: Glagolitic.Dobro, // ⰴ
-  đ: Glagolitic.Djervi, // ⰼ
-  ѕ: Glagolitic.Dzelo, // ⰷ
-  dž: Glagolitic.Dobro + Glagolitic.Zhivete, // ⰴⰶ
-  e: Glagolitic.Yestu, // ⰵ
-  ě: Glagolitic.Yati, // ⱑ
-  ę: Glagolitic.Small_Yus, // ⱔ
-  ė: Glagolitic.Yeri, // ⱐ
-  ь: Glagolitic.Yeri, // ⰴⱐ
-  f: Glagolitic.Fritu, // ⱇ
-  g: Glagolitic.Glagoli, // ⰳ
-  h: Glagolitic.Heru, // ⱈ
-  i: Glagolitic.I, // ⰻ
-  j: Glagolitic.Izhe, // ⰹ
-  '’j': Glagolitic.Initial_Izhe, // ⰺ
-  ję: Glagolitic.Iotated_Small_Yus, // ⱗ
-  jo: Glagolitic.Yo, // ⱖ
-  ju: Glagolitic.Yu, // ⱓ
-  jų: Glagolitic.Iotated_Big_Yus, // ⱙ
+function buildGlagolitizer({
+  latinateMyslite = false,
+  shta = false,
+}: Latn2GlagOptions) {
+  const id = optionsToInt({ latinateMyslite, shta });
+  if (!MEMO[id]) {
+    const glagolify = createReplacer({
+      a: Glagolitic.Azu, // ⰰ
+      å: Glagolitic.Otu, // ⱉ
+      b: Glagolitic.Buky, // ⰱ
+      c: Glagolitic.Tsi, // ⱌ
+      ć: shta ? Glagolitic.Shta : Glagolitic.Chrivi + Glagolitic.Yeri, // ⱋ | ⱍⱐ
+      č: Glagolitic.Chrivi, // ⱍ
+      d: Glagolitic.Dobro, // ⰴ
+      đ: Glagolitic.Djervi, // ⰼ
+      ѕ: Glagolitic.Dzelo, // ⰷ
+      dž: Glagolitic.Dobro + Glagolitic.Zhivete, // ⰴⰶ
+      e: Glagolitic.Yestu, // ⰵ
+      ě: Glagolitic.Yati, // ⱑ
+      ę: Glagolitic.Small_Yus, // ⱔ
+      ė: Glagolitic.Yeri, // ⱐ
+      ь: Glagolitic.Yeri, // ⰴⱐ
+      f: Glagolitic.Fritu, // ⱇ
+      g: Glagolitic.Glagoli, // ⰳ
+      h: Glagolitic.Heru, // ⱈ
+      i: Glagolitic.I, // ⰻ
+      j: Glagolitic.Izhe, // ⰹ
+      '’j': Glagolitic.Initial_Izhe, // ⰺ
+      ję: Glagolitic.Iotated_Small_Yus, // ⱗ
+      jo: Glagolitic.Yo, // ⱖ
+      ju: Glagolitic.Yu, // ⱓ
+      jų: Glagolitic.Iotated_Big_Yus, // ⱙ
+      k: Glagolitic.Kako, // ⰽ
+      l: Glagolitic.Ljudije, // ⰾ
+      m: latinateMyslite ? Glagolitic.Latinate_Myslite : Glagolitic.Myslite, // ⰿ
+      mj:
+        (latinateMyslite ? Glagolitic.Latinate_Myslite : Glagolitic.Myslite) +
+        Glagolitic.Initial_Izhe, // ⰿⰺ
+      n: Glagolitic.Nashi, // ⱀ
+      o: Glagolitic.Onu, // ⱁ
+      ȯ: Glagolitic.Yeru, // ⱏ
+      p: Glagolitic.Pokoji, // ⱂ
+      pj: Glagolitic.Pokoji + Glagolitic.Initial_Izhe, // ⱂⰺ
+      r: Glagolitic.Ritsi, // ⱃ
+      R: Glagolitic.Yeru + Glagolitic.Ritsi, // ⱏⱃ
+      s: Glagolitic.Slovo, // ⱄ
+      sj: Glagolitic.Slovo + Glagolitic.Initial_Izhe, // ⱄⰺ
+      š: Glagolitic.Sha, // ⱎ
+      t: Glagolitic.Tvrido, // ⱅ
+      tj: Glagolitic.Tvrido + Glagolitic.Initial_Izhe, // ⱅⰺ
+      θ: Glagolitic.Fita, // ⱚ
+      u: Glagolitic.Uku, // ⱆ
+      ų: Glagolitic.Big_Yus, // ⱘ
+      ù: Glagolitic.Izhitsa, // ⱛ
+      v: Glagolitic.Vedi, // ⰲ
+      vj: Glagolitic.Vedi + Glagolitic.Initial_Izhe, // ⰲⰺ
+      y: Glagolitic.Yeru + Glagolitic.Izhe, // ⱏⰹ
+      z: Glagolitic.Zemlja, // ⰸ
+      ž: Glagolitic.Zhivete, // ⰶ
+    });
 
-  // "ja": Glagolitic.Trokutasti_A, // ⱝ
-  // "ja": "ⰹⰰ", zavisi od města?
-  // "je": Glagolitic.Initial_Izhe + Glagolitic.Yestu, // ⰺⰵ
-  // "ji": Glagolitic.Initial_Izhe + Glagolitic.I, // ⰺⰻ
-  // "jy": Glagolitic.Initial_Izhe + Glagolitic.Izhe + Glagolitic.Yeru, // ⰺⰹⱏ
+    MEMO[id] = (word) => izhefy(glagolify(desoftener(syllabicR(word))));
+  }
 
-  k: Glagolitic.Kako, // ⰽ
-  l: Glagolitic.Ljudije, // ⰾ
-  m: Glagolitic.Myslite, // ⰿ
-  mj: Glagolitic.Myslite + Glagolitic.Initial_Izhe, // ⰿⰺ
-  n: Glagolitic.Nashi, // ⱀ
-  o: Glagolitic.Onu, // ⱁ
-  ȯ: Glagolitic.Yeru, // ⱏ
-  p: Glagolitic.Pokoji, // ⱂ
-  pj: Glagolitic.Pokoji + Glagolitic.Initial_Izhe, // ⱂⰺ
-  r: Glagolitic.Ritsi, // ⱃ
-  s: Glagolitic.Slovo, // ⱄ
-  sj: Glagolitic.Slovo + Glagolitic.Initial_Izhe, // ⱄⰺ
-  š: Glagolitic.Sha, // ⱎ
-  t: Glagolitic.Tvrido, // ⱅ
-  tj: Glagolitic.Tvrido + Glagolitic.Initial_Izhe, // ⱅⰺ
-  θ: Glagolitic.Fita, // ⱚ
-  u: Glagolitic.Uku, // ⱆ
-  ų: Glagolitic.Big_Yus, // ⱘ
-  ù: Glagolitic.Izhitsa, // ⱛ
-  v: Glagolitic.Vedi, // ⰲ
-  vj: Glagolitic.Vedi + Glagolitic.Initial_Izhe, // ⰲⰺ
-  y: Glagolitic.Yeru + Glagolitic.Izhe, // ⱏⰹ
-  z: Glagolitic.Zemlja, // ⰸ
-  ž: Glagolitic.Zhivete, // ⰶ
-});
-
-const latn2glag4word = (word: string) =>
-  izhefy(glagolify(ciafy(softener(word))));
-
-export function latn2glag(text: string): string {
-  return punctuate(replaceByWord(text, latn2glag4word));
+  return MEMO[id]!;
 }
 
-// Extra rules:
-// [ⱏⱐⰹ]ⰹ → [ⱏⱐⰹ]ⰺ
-// [ⱌⱄ]ⰻ[ⰰⰵⱁⱆ] → [ⱌⱄ]ⰺ[ⰰⰵⱁⱆ]
-// Pytanje: sjesti - Initial Izhe (з'їсти)
+const DEFAULT_OPTIONS: Required<Latn2GlagOptions> = {
+  latinateMyslite: false,
+  shta: true,
+};
 
-/*
-
-
-*/
+export function latn2glag(text: string, options = DEFAULT_OPTIONS): string {
+  return punctuate(replaceByWord(text, buildGlagolitizer(options)));
+}
